cedaede942e4b864076b802c513e7e88373fa3f4
Munnoi <89596902+Munnoi@users.noreply.github.com>
2025-01-05 20:23:57 +0530

    Create AboutMe.rs

---
+++ AboutMe.rs
@@ -0,0 +1,9 @@
+/*
+To compile and execute, run 'cargo run' from the terminal
+or click the 'run' button.
+*/
+fn main() {
+  println!("My name is Munno.");
+  println!("I love programming.");
+  println!("I am an avid reader.")
+}
